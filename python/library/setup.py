dab9d3a648f715d76dd7750c3ae61478c0a69b53
RogueM <chaosgen6@googlemail.com>
2015-12-27 15:49:46 +0100

    version to match pip repo

--- python/library/setup.py
+++ python/library/setup.py
@@ -38,7 +38,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Topic :: System :: Hardware']
 
 setup(name		= 'skywriter',
-	version		= '0.0.2',
+	version		= '0.0.5',
 	author		= 'Philip Howard',
 	author_email	= 'phil@pimoroni.com',
 	description	= 'Skywriter HAT Driver',
